a98ffd15abbca67a60ab5f31b2b0e84b8178a56c
Matthew Reed <matthew.reed2000@gmail.com>
2020-01-16 15:49:57 -0700

    PHP Team exercise

---
+++ web/week01/team02/team02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <style type="text/css">
+    .colored {
+      color: red;
+    }
+  </style>
+</head>
+<body>
+  <?php
+    for ($i = 0; $i < 10; $i++) {
+      echo '<div';
+      if ($i % 2 == 0) {
+        echo ' class=\"colored\"';
+      }
+      echo "><p>This is div #$i</p></div>";
+    }
+  ?>
+</body>
+</html>
