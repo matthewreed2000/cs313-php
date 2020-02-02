77007d92432f9cb824111f0eb0e98d36d1fcf020
Matthew A Reed <matthewreed2000@byui.edu>
2020-02-01 20:13:11 -0700

    Started testing values in database

--- db/prjDatabase.sql
+++ db/prjDatabase.sql
@@ -1,35 +1,58 @@
-CREATE TABLE User (
+-- User Information
+CREATE TABLE PUBLIC.User (
    ID          SERIAL         NOT NULL PRIMARY KEY,
    Username    VARCHAR(100)   NOT NULL UNIQUE,
    Password    VARCHAR(100)   NOT NULL,
    DisplayName VARCHAR(100)   NOT NULL
 );
 
-CREATE TABLE Task (
-   ID          SERIAL         NOT NULL PRIMARY KEY,
-   Title       VARCHAR(100)   NOT NULL,
-   Description TEXT,
-   Priority    INT            DEFAULT 0 
+-- Patterns for Task Repetition
+CREATE TABLE PUBLIC.Repeat (
+   ID       SERIAL      NOT NULL PRIMARY KEY,
+   Pattern  VARCHAR(10) NOT NULL,
+   EndDate  DATE
 );
 
-CREATE TABLE Repeat (
-   ID          SERIAL      NOT NULL PRIMARY KEY,
-   Pattern     VARCHAR(10) NOT NULL,
-   StartTime   SMALLINT    DEFAULT 0,
-   EndTime     SMALLINT    DEFAULT 1439,
-   StartDate   DATE        DEFAULT GETDATE(),
-   EndDate     DATE        DEFAULT GETDATE()
+-- Task
+CREATE TABLE PUBLIC.Task (
+   ID             SERIAL         NOT NULL PRIMARY KEY,
+   Title          VARCHAR(100)   NOT NULL,
+   Description    TEXT,
+   EndDateOffset  INT            DEFAULT 0,
+   StartTime      SMALLINT       DEFAULT 0,
+   EndTime        SMALLINT       DEFAULT 1439,
+   Priority       INT            DEFAULT 0,
+   RepeatID       INT            REFERENCES PUBLIC.Repeat(ID)
 );
 
-CREATE TABLE DateTask (
+-- Connects tasks to user and sets date
+CREATE TABLE PUBLIC.UserTask (
    ID       SERIAL   NOT NULL PRIMARY KEY,
-   SetDate  DATE,
-   TaskID            NOT NULL REFERENCES Task(ID),
-   RepeatID          NOT NULL REFERENCES Repeat(ID)
+   UserID   INT      NOT NULL REFERENCES PUBLIC.User(ID),
+   TaskID   INT      NOT NULL REFERENCES PUBLIC.Task(ID),
+   SetDate  DATE     DEFAULT NOW()
 );
 
-CREATE TABLE UserDateTask (
-   ID       SERIAL   NOT NULL PRIMARY KEY,
-   UserID            NOT NULL REFERENCES User(ID),
-   DateTaskID        NOT NULL REFERENCES DateTask(ID)
-);
+-- These may be changed later if I find a better way of organizing.
+
+
+-- Testing Some Values
+INSERT INTO public.User (username, password, displayname)
+VALUES ('ree18008', 'password', 'Matthew Reed');
+
+INSERT INTO public.User (username, password, displayname)
+VALUES ('brobirch', '123456', 'Bro Birch');
+
+INSERT INTO public.Task (title)
+VALUES ('Test task');
+
+-- This would be weekly on thursdays until March 03
+INSERT INTO public.Repeat (Pattern, EndDate)
+VALUES ('wR', '2020-03-03');
+
+-- This would be from 8:00 AM to 8:00 PM
+INSERT INTO public.Task (Title, Description, StartTime, EndTime)
+VALUES ('Task Thing', 'This is a test task also', 480, 1200);
+
+INSERT INTO public.UserTask (UserId, TaskID)
+VALUES (1, 2);
