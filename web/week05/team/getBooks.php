c3be9b26bc9977d5546155ffdd827270f2787c80
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-04 16:36:37 -0700

    Added Team05 stuff

--- web/week05/team/getBooks.php
+++ web/week05/team/getBooks.php
@@ -6,11 +6,11 @@
   require "../../modules/dbConnect.php";
   $db = get_db();
 
-  $book = $_POST['book'];
+  // $book = $_POST['book'];
 
-  $scriptures = $db->prepare("SELECT * FROM scripture WHERE book = '$book'");
-  $scriptures->execute();
-  while ($scripRow = $scriptures->fetch(PDO::FETCH_ASSOC)) {
-    echo "$scriptRow['book']";
-  }
+  // $scriptures = $db->prepare("SELECT * FROM scripture WHERE book = '$book'");
+  // $scriptures->execute();
+  // while ($scripRow = $scriptures->fetch(PDO::FETCH_ASSOC)) {
+  //   echo "$scriptRow['book']";
+  // }
 ?>
