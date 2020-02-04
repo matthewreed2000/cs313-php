169c8996e407c46601418131ceb6f0acbf504b3e
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-04 15:57:30 -0700

    Added some in-class stuff

--- web/in-class/week05/index.php
+++ web/in-class/week05/index.php
@@ -1,9 +1,9 @@
 <?php
-  require "dbConnect.php";
-  $db = get_db();
+  // require "dbConnect.php";
+  // $db = get_db();
 
-  $family_members = $db.prepare("SELECT * FROM week05family");
-  $family_members->execute();
+  // $family_members = $db.prepare("SELECT * FROM week05family");
+  // $family_members->execute();
 
   // while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC)) {
   //   $first_name = $fRow['first_name'];
@@ -18,4 +18,23 @@
 
   //   echo "<p>$first_name $last_name is my $relationship_id</p>";
   // }
+
+   require "dbConnect.php";
+   $db = get_db();
+   $family_members = $db->prepare("SELECT * FROM w5_family_members");
+   $family_members->execute();
+   while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC))
+   {
+      $first_name = $fRow["first_name"];
+      $last_name = $fRow["last_name"];
+      $relationship_id = $fRow["relationship_id"];
+      $relationships = $db->prepare("SELECT description FROM w5_relationships WHERE id = $relationship_id");
+      $relationships->execute();
+      while ($rRow = $relationships->fetch(PDO::FETCH_ASSOC))
+      {
+         $relationship = $rRow["description"];
+      }
+      echo "<p>$first_name $last_name is my $relationship ($relationship_id)</p>";
+   }
+
 ?>
