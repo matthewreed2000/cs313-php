d32e560f6d4feef1143a61e651613631a89440c6
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-04 15:52:07 -0700

    Added some in-class stuff

--- web/in-class/week05/index.php
+++ web/in-class/week05/index.php
@@ -2,14 +2,14 @@
   require "dbConnect.php";
   $db = get_db();
 
-  $family_members = $db.prepare("SELECT * FROM week05family;");
-  // $family_members->execute();
+  $family_members = $db.prepare("SELECT * FROM week05family");
+  $family_members->execute();
 
-  // while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC)) {
-  //   $first_name = $fRow['first_name'];
-  //   $last_name = $fRow['last_name'];
-  //   $relationship_id = $fRow['relationship'];
+  while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC)) {
+    $first_name = $fRow['first_name'];
+    $last_name = $fRow['last_name'];
+    $relationship_id = $fRow['relationship'];
 
-  //   echo "<p>$first_name $last_name is my $relationship_id</p>";
-  // }
+    echo "<p>$first_name $last_name is my $relationship_id</p>";
+  }
 ?>
