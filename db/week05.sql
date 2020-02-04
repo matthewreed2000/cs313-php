69c9e2bd7d9c588b56c22267dffa69f3fe2a6dc4
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-04 16:06:00 -0700

    Added some in-class stuff

--- db/week05.sql
+++ db/week05.sql
@@ -1,59 +1,59 @@
-DROP TABLE IF EXISTS week05family;
-DROP TABLE IF EXISTS week05rel;
+DROP TABLE IF EXISTS w5_family_members;
+DROP TABLE IF EXISTS w5_relationships;
 
-CREATE TABLE week05rel (
+CREATE TABLE w5_relationships (
    id SERIAL NOT NULL PRIMARY KEY,
    description VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE week05family (
+CREATE TABLE w5_family_members (
    id SERIAL NOT NULL PRIMARY KEY,
    first_name VARCHAR(100) NOT NULL,
    last_name VARCHAR(100) NOT NULL,
-   relationship INT NOT NULL REFERENCES week05rel(id)
+   relationship_id INT NOT NULL REFERENCES w5_relationships(id)
 );
 
-INSERT INTO week05rel (description) VALUES ('Mother');
-INSERT INTO week05rel (description) VALUES ('Father');
-INSERT INTO week05rel (description) VALUES ('Sister');
-INSERT INTO week05rel (description) VALUES ('Brother');
-INSERT INTO week05rel (description) VALUES ('In-Law');
-INSERT INTO week05rel (description) VALUES ('Niece');
-INSERT INTO week05rel (description) VALUES ('Nephew');
+INSERT INTO w5_relationships (description) VALUES ('Mother');
+INSERT INTO w5_relationships (description) VALUES ('Father');
+INSERT INTO w5_relationships (description) VALUES ('Sister');
+INSERT INTO w5_relationships (description) VALUES ('Brother');
+INSERT INTO w5_relationships (description) VALUES ('In-Law');
+INSERT INTO w5_relationships (description) VALUES ('Niece');
+INSERT INTO w5_relationships (description) VALUES ('Nephew');
 
-INSERT INTO week05family ( first_name,
+INSERT INTO w5_family_members ( first_name,
                            last_name,
-                           relationship)
+                           relationship_id)
                   VALUES ( 'Debi',
                            'Reed',
                            1);
-INSERT INTO week05family ( first_name,
+INSERT INTO w5_family_members ( first_name,
                            last_name,
-                           relationship)
+                           relationship_id)
                   VALUES ( 'Curtis',
                            'Reed',
                            2);
-INSERT INTO week05family ( first_name,
+INSERT INTO w5_family_members ( first_name,
                            last_name,
-                           relationship)
+                           relationship_id)
                   VALUES ( 'Becky',
                            'Dzierson',
                            3);
-INSERT INTO week05family ( first_name,
+INSERT INTO w5_family_members ( first_name,
                            last_name,
-                           relationship)
+                           relationship_id)
                   VALUES ( 'Melody',
                            'Richards',
                            3);
-INSERT INTO week05family ( first_name,
+INSERT INTO w5_family_members ( first_name,
                            last_name,
-                           relationship)
+                           relationship_id)
                   VALUES ( 'Michael',
                            'Reed',
                            4);
-INSERT INTO week05family ( first_name,
+INSERT INTO w5_family_members ( first_name,
                            last_name,
-                           relationship)
+                           relationship_id)
                   VALUES ( 'Lisa',
                            'Dzierson',
                            3);
--- web/in-class/week05/index.php
+++ web/in-class/week05/index.php
@@ -19,16 +19,17 @@
   //   echo "<p>$first_name $last_name is my $relationship_id</p>";
   // }
 
+try {
    require "dbConnect.php";
    $db = get_db();
-   $family_members = $db->prepare("SELECT * FROM week05family");
+   $family_members = $db->prepare("SELECT * FROM w5_family_members");
    $family_members->execute();
    while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC))
    {
       $first_name = $fRow["first_name"];
       $last_name = $fRow["last_name"];
-      $relationship_id = $fRow["relationship"];
-      $relationships = $db->prepare("SELECT description FROM week05rel WHERE id = $relationship_id");
+      $relationship_id = $fRow["relationship_id"];
+      $relationships = $db->prepare("SELECT description FROM w5_relationships WHERE id = $relationship_id");
       $relationships->execute();
       while ($rRow = $relationships->fetch(PDO::FETCH_ASSOC))
       {
@@ -36,5 +37,8 @@
       }
       echo "<p>$first_name $last_name is my $relationship ($relationship_id)</p>";
    }
-
+}
+catch (\Exception $ex) {
+  echo "TEST";
+}
 ?>
