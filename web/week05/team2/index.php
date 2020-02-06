eaeb20b7c108a5631e3cf3649acf68a70c7335ae
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-06 16:33:50 -0700

    Added Week05/team2 stuff

--- web/week05/team2/index.php
+++ web/week05/team2/index.php
@@ -6,16 +6,13 @@
   require "../../modules/dbConnect.php";
   $db = get_db();
 
-  $statement = $db->prepare("SELECT name FROM w5_EVENT");
+  $statement = $db->prepare("SELECT name, image FROM w5_EVENT");
   $statement->execute();
 
   while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-    $name = $row['name'];
-    $image = $row['image'];
-
-    echo "<p>" . $name . "</p>";
-    echo "<img src=\"";
-    echo $image;
-    echo "\" />";
+?>
+    <p><?=$row['name']?></p>
+    <img src="<?=$row['image']?>" />
+<?php
   }
 ?>
