d09a32e42d8a116851a228037f16fd68d2bf5ba1
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-05 20:58:19 -0700

    Added Team05 stuff

--- web/week05/team/index.php
+++ web/week05/team/index.php
@@ -45,6 +45,7 @@
          var xhttp = new XMLHttpRequest();
          xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
+               console.log(this.responseText);
                content = document.getElementById('content');
                var obj = JSON.parse(this.responseText);
                content.innerHTML = '';
