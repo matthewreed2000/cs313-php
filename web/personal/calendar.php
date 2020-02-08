c9a6e1128c9f1db7ca8bd1dc5b4d2d61a3653957
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-08 16:28:14 -0700

    Worked on calendar page

--- web/personal/calendar.php
+++ web/personal/calendar.php
@@ -52,7 +52,7 @@
     }
 
     for($i = 1; $i < $numdays + 1; $i++) {
-      $date = date("Y_M_D", $i);
+      $date = date("Y_m_", $i) . sprintf("%2d", $i);
       echo $date;
       $statement = $db->prepare("
         SELECT t.Title FROM PUBLIC.Task t
