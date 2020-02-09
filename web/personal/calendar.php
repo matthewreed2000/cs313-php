883f0f58f8b37dc5d6cf9afa2c167f29c4184f69
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-08 20:56:04 -0700

    Made calendar display in grids

--- web/personal/calendar.php
+++ web/personal/calendar.php
@@ -38,8 +38,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Calendar</title>
-  <link rel="stylesheet" src="css/calendar.css">
+  <link rel="stylesheet" href="css/calendar.css">
 </head>
 <body>
   <div class="calendarHolder">
