fa5e79e7ac8691f31a6184c1b619c9fe3cd8e299
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-11 15:49:21 -0700

    In-class week06 stuff

--- web/week06/team01/display.php
+++ web/week06/team01/display.php
@@ -1,4 +1,8 @@
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
 	require("dbConnect.php");
 	$db = get_db();
 ?>
@@ -6,16 +10,18 @@
 		<div class="container">
          <?php
             $personId = $_GET['personId'];
-            $statement = $db->prepare("SELECT * FROM w6_user WHERE ID = :personId");
+            $statement = $db->prepare("SELECT * FROM w6_user WHERE Id = :personId");
             $statement->bindValue(':personId', $personId);
             $statement->execute();
+
+            $food = 0;
             while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
               $id = $row['id'];
               $first = $row['first_name'];
               $last = $row['last_name'];
               $food_id = $row['food_type'];
 
-              $foods = $db->prepare("SELECT * FROM w6_food WHERE ID = :food_id");
+              $foods = $db->prepare("SELECT * FROM w6_food WHERE Id = :food_id");
               $foods->bindValue(':food_id', $food_id);
               $foods->execute();
               while ($food_row = $statement->fetch(PDO::FETCH_ASSOC)) {
