e2a3751f5993f393617358b42f122b5f80924949
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-13 16:45:54 -0700

    Worked on adding tasks

--- web/personal/addTask.php
+++ web/personal/addTask.php
@@ -42,8 +42,8 @@
   // Will add task if POST exists
   if (isset($_POST)) {
     // TODO
-    $validForm = isset($_POST['title']) ? !isempty($_POST['title']) : 0 &&
-                 isset($_POST['descr']) ? !isempty($_POST['descr']) : 0;
+    $validForm = isset($_POST['title']) ? !empty($_POST['title']) : 0 &&
+                 isset($_POST['descr']) ? !empty($_POST['descr']) : 0;
     if ($validForm) {
       $title = sanitizeInput($_POST['title']);
       $descr = sanitizeInput($_POST['descr']);
