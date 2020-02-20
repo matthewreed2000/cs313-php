3c1f78b98ef78d6880c944821a618604df8ca993
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-20 15:29:53 -0700

    Worked on team07

---
+++ web/week07/team01/index.php
@@ -0,0 +1,14 @@
+<?php
+  
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <input type="text" name="username" placeholder="Username" />
+  <input type="password" name="password" placeholder="Password" />
+</body>
+</html>
