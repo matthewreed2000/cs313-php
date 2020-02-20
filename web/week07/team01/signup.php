8abc7badb7e47f6fb8687baa3bd5169c4b33d37a
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-20 15:40:46 -0700

    Worked on team07

--- web/week07/team01/signup.php
+++ web/week07/team01/signup.php
@@ -1,5 +1,11 @@
 <?php
-  
+  if (isset($_POST['username'] && isset($_POST['password']))) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    echo "$username and $hash";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +14,7 @@
   <title></title>
 </head>
 <body>
-  <form>
+  <form action="" method="POST">
     <input type="text" name="username" placeholder="Username" />
     <input type="password" name="password" placeholder="Password" />
     <button type="submit">Submit</button>
