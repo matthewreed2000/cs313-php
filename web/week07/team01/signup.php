8231291905b3df5db7edf3fb6b8a678a3a8c8c92
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-20 21:33:31 -0700

    Worked on team07

--- web/week07/team01/signup.php
+++ web/week07/team01/signup.php
@@ -6,20 +6,28 @@
   require "../../modules/dbConnect.php";
   $db = get_db();
 
+  $mismatch = false;
+
   if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $passmatch = $_POST['passmatch'];
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $query = 'INSERT INTO week07_user(username, password) VALUES (:username, :password) ON CONFLICT DO NOTHING';
-    $stmnt = $db->prepare($query);
-    $stmnt->bindValue(':username', $username);
-    $stmnt->bindValue(':password', $hash);
-    $stmnt->execute();
+    if ($password == $passmatch) {
+      $query = 'INSERT INTO week07_user(username, password) VALUES (:username, :password) ON CONFLICT DO NOTHING';
+      $stmnt = $db->prepare($query);
+      $stmnt->bindValue(':username', $username);
+      $stmnt->bindValue(':password', $hash);
+      $stmnt->execute();
 
-    // echo "$username and $hash";
-    header('Location: signin.php');
-    die();
+      // echo "$username and $hash";
+      header('Location: signin.php');
+      die();
+    }
+    else {
+      $mismatch = true;
+    }
   }
 ?>
 
@@ -32,7 +40,11 @@
   <form action="" method="POST">
     <input type="text" name="username" placeholder="Username" />
     <input type="password" name="password" placeholder="Password" />
+    <input type="password" name="passmatch" placeholder="Re-type Password" />
     <button type="submit">Submit</button>
   </form>
+  <?php if ($mismatch) { ?>
+    <p>Passwords do not match</p>
+  <?php } ?>
 </body>
 </html>
