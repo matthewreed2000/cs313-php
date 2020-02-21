508614c63e74d29f8c362c794f6bca4f1bd74a1c
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-20 22:17:07 -0700

    Worked on team07

--- web/week07/team01/signup.php
+++ web/week07/team01/signup.php
@@ -57,7 +57,7 @@
   <form action="" method="POST">
     <input type="text" name="username" placeholder="Username" />
     <br />
-    <input type="password" name="password" placeholder="Password" />
+    <input type="password" name="password" placeholder="Password" oninput="pass_strength(this);"/>
     <?php if ($error != NULL) { ?>
       <p>*</p>
     <?php } ?>
