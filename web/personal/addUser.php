ac6df3ebc1842bf1477a26cc8861f61a5fb51850
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-22 18:33:26 -0700

    Added Link to AddUser page

--- web/personal/addUser.php
+++ web/personal/addUser.php
@@ -80,19 +80,15 @@
   <main id="content-wrap">
     <form action="" method="POST">
       <input type="text" name="username" placeholder="Username" required />
-      <br />
       <input type="password" name="password" placeholder="Password" required />
       <?php if ($error != NULL) { ?>
         <p>*</p>
       <?php } ?>
-      <br />
       <input type="password" name="passmatch" placeholder="Re-type Password" required />
       <?php if ($error != NULL) { ?>
         <p>*</p>
       <?php } ?>
-      <br />
       <input type="text" name="display" placeholder="Display Name" required />
-      <br />
       <button type="submit">Submit</button>
     </form>
     <p id='output'></p>
