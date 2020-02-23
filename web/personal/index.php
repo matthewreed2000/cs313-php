6b91559303f3386eacb4692985c14a509f724472
Matthew Reed <matthew.reed2000@gmail.com>
2020-02-22 21:58:23 -0700

    Fixed footer

--- web/personal/index.php
+++ web/personal/index.php
@@ -24,43 +24,45 @@
   <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-  <?php include "modules/header.php";?>
-  <main id="content-wrap">
-    <div class="jumbotron">
-      <?php
-        if (isset($_POST['username']) && isset($_POST['password'])) {
-          $username = sanitizeInput($_POST['username']);
-          $password = sanitizeInput($_POST['password']);
+  <div id="page-container">
+    <?php include "modules/header.php";?>
+    <main id="content-wrap">
+      <div class="jumbotron">
+        <?php
+          if (isset($_POST['username']) && isset($_POST['password'])) {
+            $username = sanitizeInput($_POST['username']);
+            $password = sanitizeInput($_POST['password']);
 
-          // $statement = $db->prepare("SELECT password FROM UserData
-          //   WHERE username='$username'");
-          // $statement->execute();
+            // $statement = $db->prepare("SELECT password FROM UserData
+            //   WHERE username='$username'");
+            // $statement->execute();
 
-          // $dbPass = $statement->fetch(PDO::FETCH_ASSOC)['password'];
-          if (verify_password('UserData', $username, $password)) {
-            $_SESSION['frozen_waters_username'] = $username;
-            $_SESSION['frozen_waters_password'] = $password;
+            // $dbPass = $statement->fetch(PDO::FETCH_ASSOC)['password'];
+            if (verify_password('UserData', $username, $password)) {
+              $_SESSION['frozen_waters_username'] = $username;
+              $_SESSION['frozen_waters_password'] = $password;
 
-            header("Location: calendar.php", true, 301);
-            exit();
+              header("Location: calendar.php", true, 301);
+              exit();
+            }
+            else {
+              $show_error = true;
+            }
           }
-          else {
-            $show_error = true;
-          }
-        }
-      ?>
-      <form action="" method="POST">
-        <input type="username" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Log In</button>
-      </form>
-      <a href="addUser.php">Register Account</a>
-      <p>For testing purposes, use the username "test" and the password "test123"</p>
-      <?php if ($show_error) { ?>
-        <p>Incorrect username or password</p>
-      <?php } ?>
-    </div>
-  </main>
-  <?php include "modules/footer.php";?>
+        ?>
+        <form action="" method="POST">
+          <input type="username" name="username" placeholder="Username" required>
+          <input type="password" name="password" placeholder="Password" required>
+          <button type="submit">Log In</button>
+        </form>
+        <a href="addUser.php">Register Account</a>
+        <p>For testing purposes, use the username "test" and the password "test123"</p>
+        <?php if ($show_error) { ?>
+          <p>Incorrect username or password</p>
+        <?php } ?>
+      </div>
+    </main>
+    <?php include "modules/footer.php";?>
+  </div>
 </body>
 </html>
